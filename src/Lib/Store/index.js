ceb72db18207d49f3e1514037cdfa4e3dfdc235e
Asad-ijaz <iasad5870@gmail.com>
2024-07-26 13:41:52 +0500

    done

--- src/Lib/Store/index.js
+++ src/Lib/Store/index.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { rootReducer } from "./Slices";
+import { rootReducer } from "./Slices/index";
 
 export const store = configureStore({
   reducer: rootReducer,
